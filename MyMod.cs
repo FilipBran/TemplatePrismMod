40e03afeaf3144937bc5c495fe27242b71406344
Filip Brański <filip.branski2010@gmail.com>
2026-02-22 17:52:02 +0100

    Initial commit

---
+++ MyMod.cs
@@ -0,0 +1,15 @@
+﻿using Prism;
+
+namespace TemplatePrismMod;
+
+public sealed class MyMod : Mod
+{
+    public override string Name => "MyMod";
+    public override string Version => "1.0.0";
+    public override string Id => "com.example.mymod";
+
+    public override void Init()
+    {
+        AdvancedLogger.Log($"[{Name}] Loaded mod {Name}!", AdvancedLogger.LogType.Info);
+    }
+}
